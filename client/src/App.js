1955b202cfd9996889e4716c60290bb9d3d73653
Pboerma <boerma.pieter@gmail.com>
2021-03-18 17:44:24 -0400

    updated appjs with signup

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Splash from "./pages/Splash";
 import Main from "./pages/Main/Main";
+import SignUp from "./pages/SignUp";
 import "./App.css";
 import "materialize-css/dist/css/materialize.min.css";
 
@@ -11,7 +12,7 @@ function App() {
             <Switch>
                 <Route exact path="/" component={Splash} />
                 <Route exact path="/home" component={Main} />
-                {/* <Route exact path="/signup" component={Signup} /> */}
+                <Route exact path="/signup" component={Signup} />
                 {/* <Route exact path="/login" component={Login} /> */}
             </Switch>
         </Router>
