3b2ccef1c97951e198367014c16ebfd436c03d1a
Manuel Leung Chen <manuel.leungchen@gmail.com>
2021-03-18 00:26:13 -0400

    Added a Splash page

---
+++ client/src/pages/Splash/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./style.css";
+
+const Splash = () => {
+
+    const he = window.innerHeight;
+
+    return (
+        <main id="main-splash" style={{ height: `${he}px` }}>
+            <section id="close-button">
+                <a href="/home">
+                    <i class="medium material-icons" style={{ color: "white" }} >close</i>
+                </a>
+            </section>
+
+            <section id="logo"><img src="../../../img/puppy-love.png" /></section>
+
+            <section id="message">
+                <h5>Discover new friends for your lovely pets around your area.</h5>
+                <p>You must have a pet to enjoy this app.</p>
+                <p>This application requires <b>Location Service</b> to be ON</p>
+            </section>
+        </main>
+    )
+}
+
+export default Splash
