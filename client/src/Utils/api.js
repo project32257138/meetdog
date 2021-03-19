ba163d0e952f7d5aed82f4d462a4be6777247ac9
Natasha Fray <natasha.fray9@gmail.com>
2021-03-19 16:04:50 -0400

    now produces endless stream of unique dogs (hopefully)

--- client/src/Utils/api.js
+++ client/src/Utils/api.js
@@ -5,7 +5,8 @@ let dogIndex = 0
 
 const API = {
     
-    getNextDogNoCheck: function(cb) {
+    getNextDog: function(cb) {
+        console.log("here2")
         axios.get("https://dog.ceo/api/breeds/image/random/")
         .then(data => {
             console.log(data)
@@ -16,18 +17,21 @@ const API = {
         })
     },
 
-    // getNextDogs(n) {
-    //     while (dogList.length < n + dogIndex) {
-    //         axios.get("https://dog.ceo/api/breeds/image/random/")
-    //         .then(data => data.data.message)
-    //         .then(nextDog => {
-    //             if (!dogList.includes(nextDog)) dogList.push(nextDog)
-    //             return dogList
-    //         })
-    //     } return dogList
-    // },
+    getNextDogsNoCheck: function(n,cb) {
+        axios.get("https://dog.ceo/api/breeds/image/random/" + n)
+        .then(data => {
+            console.log(data)
+            return data.data.message
+        })
+        .then(nextDogs => {
+            return cb(nextDogs)
+        })
+    },
 
-    // async getNextDog() {
+
+
+
+    // getNextDog: function() {
 
     //     if (dogIndex === 0) return API.getNextDogsNoCheck(10)
     //     else if (dogIndex > dogList.length) {
--- client/src/components/Swipe/index.js
+++ client/src/components/Swipe/index.js
@@ -2,22 +2,64 @@ import React, { useState, useEffect, Component } from 'react';
 import "./index.css"
 import API from "../../utils/api"
 
-
-
 class Swipe extends Component {
 
     state = {
-        image: process.env.PUBLIC_URL + "/img/dog-05.jpeg",
+        currentDog: {
+            image: process.env.PUBLIC_URL + "/img/dog-05.jpeg",
+            name: "Doge"
+        },
         dogList: [] ,
         dogIndex: 0,
     }
 
-    getNextDog = () => {
-        return API.getNextDogNoCheck((dog) => {
-            this.setState({image: dog})
+    componentDidMount() {
+        API.getNextDogsNoCheck(10,(dogs) => {
+            this.setState({dogList : dogs})
+            debugger;       
         })
     }
 
+    getNextDogs(n,cb,list=[]) {
+        console.log(this.state.dogList.length, this.state.dogIndex)
+        // couldn't do a loop with callbacks, so I had to do this (a.k.a recursion)
+        // n will descrease each time a new dog populates
+        if (n) {
+            API.getNextDog((nextDog) => {
+                console.log(this.state.dogList.includes(nextDog))
+                if (!this.state.dogList.includes(nextDog)) {
+                    list.push(nextDog)
+                    console.log(list)
+                    console.log(n)
+                    // only decreases n if there is no match
+                    n--
+                }
+                return this.getNextDogs(n,cb,list)
+            })
+        } else return cb(list)
+    }
+
+    getNextDog = () => {
+        console.log(this.state.dogIndex, this.state.dogList.length)
+        if (this.state.dogIndex + 3 === this.state.dogList.length) {
+            this.getNextDogs(5,(dogs) => {
+                console.log(this.state.dogList,dogs)
+                this.setState({
+                    dogList: [...this.state.dogList,...dogs]
+                })
+                console.log(dogs) 
+            })
+        } 
+        this.setState({ 
+            currentDog: {
+                image: this.state.dogList[this.state.dogIndex],
+                name: "Lucky"+this.state.dogIndex,
+                },
+            dogIndex: this.state.dogIndex + 1
+        })
+        console.log(this.state)
+    }
+ 
     render() {
         return (
             <div class="row">
@@ -26,9 +68,9 @@ class Swipe extends Component {
                 <div class="card-image dog-image">
                     <i class="material-icons back">arrow_back_ios</i>
                     <i class="material-icons forward" onClick={this.getNextDog}>arrow_forward_ios</i>
-                    <img src={this.state.image}>
+                    <img src={this.state.currentDog.image}>
                     </img>
-                    <span className="card-title dog-name">Boxer</span>
+                    <span className="card-title dog-name">{this.state.currentDog.name}</span>
                 </div>
                 <div className="card-action bark-back">
                 <a href="#"><span className="material-icons like">thumb_up</span></a>
