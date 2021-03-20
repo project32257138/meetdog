96b89acd39061ea06c4c0812d527d3dbcdd70618
Cayman Heng <hengcayman@gmail.com>
2021-03-20 16:09:51 -0400

    imported auth0 provider with history to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,19 +5,22 @@ import Main from "./pages/Main/Main";
 import Welcome from "./pages/Welcome/Welcome";
 import "./App.css";
 import "materialize-css/dist/css/materialize.min.css";
+import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
 
 function App() {
-    return (
-        <Router>
-            <Switch>
-                <Route exact path="/" component={Splash} />
-                <Route exact path="/home" component={Main} />
-                <Route exact path="/welcome" component={Welcome} />
-                {/* <Route exact path="/signup" component={Signup} /> */}
-                {/* <Route exact path="/login" component={Login} /> */}
-            </Switch>
-        </Router>
-    );
+  return (
+    <Router>
+      <Auth0ProviderWithHistory>
+        <Switch>
+          <Route exact path="/" component={Splash} />
+          <Route exact path="/home" component={Main} />
+          <Route exact path="/welcome" component={Welcome} />
+          {/* <Route exact path="/signup" component={Signup} /> */}
+          {/* <Route exact path="/login" component={Login} /> */}
+        </Switch>
+      </Auth0ProviderWithHistory>
+    </Router>
+  );
 }
 
 export default App;
