3fca23c716fc903e0d30c3d019a041a555dda9d2
Natasha Fray <natasha.fray9@gmail.com>
2021-03-21 16:13:42 -0400

    minor changes

--- client/src/components/Swipe/index.js
+++ client/src/components/Swipe/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, Component } from 'react';
 import "./index.css"
-import API from "../../utils/api"
+import API from "../../utils/api.js"
 
 class Swipe extends Component {
 
