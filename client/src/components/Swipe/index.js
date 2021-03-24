2e4bda0972522a1d8fc3db9f65df173b07143da1
Natasha Fray <natasha.fray9@gmail.com>
2021-03-24 10:49:46 -0400

    just realized I'd prev liked all the dogs in advance

--- client/src/components/Swipe/index.js
+++ client/src/components/Swipe/index.js
@@ -30,15 +30,6 @@ class Swipe extends Component {
         // lastRatedIndex: -1,
         liked: {
             // object storing id and if that dog was liked by the user
-            1: true,
-            2: true,
-            3: true,
-            4: true,
-            5: true,
-            6: true,
-            7: true,
-            8: true,
-            9: true,
             // {id: true}  for liked --or--
             // {id: false} for disliked
         }
