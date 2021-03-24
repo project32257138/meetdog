e23f14ae84efca65e8945cdfa36ca0dd1d328bbd
Cayman Heng <hengcayman@gmail.com>
2021-03-24 11:35:09 -0400

    removed redundant routes in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Splash from "./pages/Splash";
 import Main from "./pages/Main/Main";
-import Welcome from "./pages/Welcome/Welcome";
 import Profile from "./pages/Profile";
 import "./App.css";
 import "materialize-css/dist/css/materialize.min.css";
@@ -14,7 +12,6 @@ function App() {
     <Router>
       <Auth0ProviderWithHistory>
         <Switch>
-          {/* <Route exact path="/" component={Splash} /> */}
           <Route exact path="/" component={Main} />
           <PrivateRoute exact path="/profile" component={Profile} />
         </Switch>
