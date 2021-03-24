fdd5b9435835df3121a8b71b051a9fe7762f21b4
Cayman Heng <hengcayman@gmail.com>
2021-03-24 10:57:56 -0400

    implemented private route on profile page

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Profile from "./pages/Profile";
 import "./App.css";
 import "materialize-css/dist/css/materialize.min.css";
 import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 
 function App() {
   return (
@@ -15,10 +16,7 @@ function App() {
         <Switch>
           {/* <Route exact path="/" component={Splash} /> */}
           <Route exact path="/" component={Main} />
-          <Route exact path="/profile" component={Profile} />
-          {/* <Route exact path="/welcome" component={Welcome} /> */}
-          {/* <Route exact path="/signup" component={Signup} /> */}
-          {/* <Route exact path="/login" component={Login} /> */}
+          <PrivateRoute exact path="/profile" component={Profile} />
         </Switch>
       </Auth0ProviderWithHistory>
     </Router>
