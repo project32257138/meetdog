d0a3099e1a9bf722748f1d6882e332e51b8e98ca
Manuel Leung Chen <manuel.leungchen@gmail.com>
2021-03-27 12:03:45 -0400

    removing style for profile page

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7a013645dcfb9dd68a1547be624b2929.js"
+  "/precache-manifest.1f83407530c57503f47c9a423e019337.js"
 );
 
 self.addEventListener('message', (event) => {
